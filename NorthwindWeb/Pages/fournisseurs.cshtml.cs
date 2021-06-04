8ce1b928bfef6c73e5a9c5e4a6f70413881130c4
Michel Nsimba Matondo <nsimba_matondo@hotmail.com>
2021-06-04 17:12:59 +0200

    first commit

---
+++ NorthwindWeb/Pages/fournisseurs.cshtml.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+
+namespace NorthwindWeb.Pages
+{
+    public class FournisseursModel : PageModel
+    {
+        public IEnumerable<string> Fournisseurs { get; set; }
+
+        public void OnGet()
+        {
+            ViewData["Title"] = "Northwind Web Site - Fournisseurs";
+
+            Fournisseurs = new[]{
+                "Alpha Co", "Beta Limited", "Gamma Group"
+            };
+        }
+    }
+}
