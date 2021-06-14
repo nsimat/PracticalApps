05d93c75027f0f0b95120d1bdd8fa7c9ba566454
Michel Nsimba Matondo <nsimba_matondo@hotmail.com>
2021-06-14 10:49:19 +0200

    Adding a SimpleWeb Project transforming a console app to web app

--- SimpleWeb/Program.cs
+++ SimpleWeb/Program.cs
@@ -1,12 +1,13 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
 
-namespace SimpleWeb
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+Host.CreateDefaultBuilder(args)
+ .ConfigureWebHostDefaults(webBuilder => 
+ {
+     webBuilder.Configure(app => 
+     {
+         app.Run(context => context.Response.WriteAsync("Hello World Wide Web!"));
+     });
+ }).Build().Run();
